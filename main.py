7b2244ebcd5ff09251b4ad2003ed4bd90cfe9620
arpeggi-rpg <148833753+arpeggi-rpg@users.noreply.github.com>
2024-01-12 00:00:03 +0000

    Update main.py
    New sprite-based approach, highlighting mostly works now

--- main.py
+++ main.py
@@ -10,43 +10,29 @@ pygame.display.set_caption('Pixel Art')
 screen.fill(BACKGROUND_COLOUR)
 pygame.display.flip()
 
-colour = (255, 0, 0)
+RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+TINT = numpy.array([50, 255, 50])
+sprite_list = pygame.sprite.Group()
 
 
-class PixelArt(pygame.sprite.Sprite):
-    def __init__(self):
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, colour, height, width):
         pygame.sprite.Sprite.__init__(self)
-        self.pixels = numpy.array([[colour for _ in range(64)] for _ in range(64)])
-        self.canvas = pygame.Rect(10, 10, 513, 513)
-        self.pixel_array = pygame.PixelArray(screen)
+        self.image = pygame.Surface([width, height])
+        pygame.draw.rect(self.image, colour, pygame.Rect(0, 0, width, height))
+        self.rect = self.image.get_rect()
 
-    def drawpixels(self):
-        for x in range(64):
-            for y in range(64):
-                self.pixels[x][y] = colour
-                self.pixel_array[self.canvas.left:self.canvas.right, self.canvas.top: self.canvas.bottom] = colour
-        self.pixel_array.close()
-        pygame.display.flip()
-
-    def updatepixel(self, x, y):
-        self.pixels[x][y] = BLUE
-        pixel_x = self.canvas.left + x*8
-        pixel_y = self.canvas.top + y*8
-        self.pixel_array[pixel_x:pixel_x + 8, pixel_y:pixel_y + 8] = self.pixels[x][y]
-        pygame.display.flip()
-
-    def highlight(self, x, y):
-        pixel_x = (x // 8) * 8
-        pixel_y = (y // 8) * 8
-        pixel_highlight = pygame.Rect(pixel_x, pixel_y, 8, 8)
+        self.colour = colour
 
 
-
-
-
-pixel_art = PixelArt()
-pixel_art.drawpixels()
+pixel = [[x for x in range(64)] for y in range(64)]
+for x in range(64):
+    for y in range(64):
+        pixel[x][y] = Sprite(RED, 8, 8)
+        pixel[x][y].rect.x = 10 + (x * 8)
+        pixel[x][y].rect.y = 10 + (y * 8)
+        sprite_list.add(pixel[x][y])
 
 running = True
 while running:
@@ -55,9 +41,26 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    if pixel_art.canvas.left < mouse_x < pixel_art.canvas.right and pixel_art.canvas.top < mouse_y < pixel_art.canvas.bottom:
-        pixel_art.highlight(mouse_x, mouse_y)
+    try:
+        if not highlight.rect.collidepoint(pygame.mouse.get_pos()):
+            sprite_list.remove(highlight)
+    except NameError:
+        pass
+
+    for x in pixel:
+        for sp in x:
+            if sp.rect.collidepoint(pygame.mouse.get_pos()):
+                highlight_colour = sp.colour + TINT
+                for i, v in enumerate(highlight_colour):
+                    if v > 255:
+                        highlight_colour[i] = 255
+                highlight = Sprite(highlight_colour, 8, 8)
+                highlight.rect.x = sp.rect.x
+                highlight.rect.y = sp.rect.y
+                sprite_list.add(highlight)
+
+    sprite_list.update()
+    sprite_list.draw(screen)
 
     pygame.draw.rect(screen, (255, 255, 255), ((10, 10), (513, 513)), 1)
     for x in range(18, 522, 8):
